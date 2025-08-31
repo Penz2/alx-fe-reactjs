77e876aa088d5f875dd4b9b927431f2f5f50282a
Mpumelelo <penzmtshali@gmail.com>
2025-08-31 21:04:04 +0200

    welcomeMessage.js to welcomeMessage.jsx and updated App.jsx to import the renamed file correctly.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './src/components/WelcomeMessage.js'
+import WelcomeMessage from './src/components/WelcomeMessage.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
