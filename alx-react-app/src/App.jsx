35069116b043614a1f07dbc9faccc6e2fed693ee
Mpumelelo <penzmtshali@gmail.com>
2025-09-01 17:25:42 +0200

    commit changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,4 @@
 import WelcomeMessage from './src/components/WelcomeMessage.jsx'
-import Header from './src/components/Header.jsx'
-import MainContent from './src/components/MainContent.jsx'
-import Footer from './src/components/Footer.jsx'
 import UserProfile from './src/components/UserProfile.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './src/components/Header.jsx'
+import MainContent from './src/components/MainContent.jsx'
+import Footer from './src/components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+  <App />
+    <Header></Header>
+    <MainContent></MainContent>
+    <Footer></Footer>
+  </>,
 )
