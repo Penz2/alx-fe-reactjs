ea7b96419fa957557d570839e1c69d290ce25c17
Mpumelelo <penzmtshali@gmail.com>
2025-09-01 17:29:16 +0200

    changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 import WelcomeMessage from './src/components/WelcomeMessage.jsx'
 import UserProfile from './src/components/UserProfile.jsx'
+ import Header from './src/components/Header.jsx'
+import MainContent from './src/components/MainContent.jsx'
+import Footer from './src/components/Footer.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -2,15 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './src/components/Header.jsx'
-import MainContent from './src/components/MainContent.jsx'
-import Footer from './src/components/Footer.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <>
   <App />
-    <Header></Header>
-    <MainContent></MainContent>
-    <Footer></Footer>
   </>,
 )
