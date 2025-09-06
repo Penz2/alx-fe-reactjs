a71930afd0c96344fa990e8becb13865b61ec88b
Mpumelelo <penzmtshali@gmail.com>
2025-09-06 17:36:23 +0200

    re-installing dependencies.

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <p style={{ fontWeight: 'opens sans' }}>© 2023 City Lovers</p>
     </footer>
   );
 }
