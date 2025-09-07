98861a7a39f55946aea02a787f279d78ab680e68
Mpumelelo <penzmtshali@gmail.com>
2025-09-07 18:20:44 +0200

    see changes staged for commit.

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
-import About from "./components/About.jsx";
-import Services from "./components/Services.jsx";
-import Contact from "./components/Contact.jsx";
+import Home from "./components/Home.jsx';
+import About from "./components/About.jsx';
+import Services from "./components/Services.jsx';
+import Contact from "./components/Contact.jsx';
 
 function App() {
   return (
