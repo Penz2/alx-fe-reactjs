b1cac56ed6fe16cd2c02a30bd5015b057ea9fc8e
Mpumelelo <penzmtshali@gmail.com>
2025-09-07 17:18:47 +0200

    initial commit message

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,27 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage.jsx';
-import UserProfile    from './components/UserProfile.jsx';
-import Header         from './components/Header.jsx';
-import MainContent    from './components/MainContent.jsx';
-import Footer         from './components/Footer.jsx';
-import { useState }   from 'react';
-import Counter        from './components/Counter.jsx';
- 
+// src/App.jsx
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <UserProfile name="Bob" age="30" bio="Enjoys coding and traveling" />
-      <UserProfile name="Charlie" age="28" bio="Passionate about music and art" />
-      <Counter count={count} setCount={setCount} />
-    </>
-  )
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
+
 
--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-      <p style={{ fontWeight: 'opens sans' }}>© 2023 City Lovers</p>
-    </footer>
-  );
-}
-
-export default Footer;
