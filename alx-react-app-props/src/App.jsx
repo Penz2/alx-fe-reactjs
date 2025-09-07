c5740dbbb277bffc0549368de2d62a4bdb4b9219
Mpumelelo <penzmtshali@gmail.com>
2025-09-07 17:28:04 +0200

    UserProfile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,20 @@
 // src/App.jsx
 import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
+    <>
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <UserProfile name="Bob" age="30" bio="Enjoys coding and traveling" />
+    <UserProfile name="Charlie" age="28" bio="Passionate about music and art" />
+    </>
   );
 }
 
