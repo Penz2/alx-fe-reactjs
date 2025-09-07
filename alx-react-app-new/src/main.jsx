a622b6986ba775094d5084908bd392d3097a48a6
Mpumelelo <penzmtshali@gmail.com>
2025-09-07 15:47:01 +0200

    commit changes on main.jsx and index.css

--- alx-react-app-new/src/main.jsx
+++ alx-react-app-new/src/main.jsx
@@ -1,14 +1,11 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { StrictMode} from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-
+import './index.css'   // <-- this links your CSS, not JS logic
 
 createRoot(document.getElementById('root')).render(
-
-    <StrictMode>
+  <React.StrictMode>
     <App />
-    </StrictMode>
-
+  </React.StrictMode>
 )
+
