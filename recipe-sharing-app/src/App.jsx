7dfdbdd68f386e69b06285601f1bcccff7b29733
Mpumelelo <penzmtshali@gmail.com>
2025-09-12 17:25:12 +0200

    error due to  extra '

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/Recipelist''
+import RecipeList from './components/Recipelist'
 import Store from './components/Store'
 import './App.css'
 
