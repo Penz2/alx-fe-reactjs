c638f4a7a6d759aec5cfe01e6710cfd95a3f3b69
Mpumelelo <penzmtshali@gmail.com>
2025-09-14 13:36:51 +0200

    add RecipeList

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import Store from './components/recipeStore'
+import RecipeList from './components/Recipelist'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-     
+     <RecipeList/>
      <AddRecipeForm /> 
       <Store />
        
