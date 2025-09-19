3bfb49d4ee5ddca1bc4968707bd468a83097f387
Mpumelelo <penzmtshali@gmail.com>
2025-09-19 18:24:57 +0200

    added a .

--- github-user-search/src/components/UserCard.jsx
+++ github-user-search/src/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchUserData } from "./services/githubService";
+import { fetchUserData } from "../services/githubService";
 
 export default function UserCard() {
   const { username } = useParams();
