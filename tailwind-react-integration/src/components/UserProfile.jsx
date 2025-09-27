e48382455fd12ff822804a977484e612d99d5a45
Mpumelelo <penzmtshali@gmail.com>
2025-09-27 14:06:15 +0200

    commit styling css on userprofile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile">
+    <div className="user-profile" >
       <img src="https://via.placeholder.com/150" alt="User" />
       <h1>John Doe</h1>
       <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
