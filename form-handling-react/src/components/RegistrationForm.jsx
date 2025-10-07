b28aa5d694b5abcfd38dba552a1f2fcbd1a6da6a
Mpumelelo <penzmtshali@gmail.com>
2025-10-07 17:35:14 +0200

    setErrors to RegistrationForm

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -14,10 +14,10 @@ function RegistrationForm() {
     if (!email) newErrors.email = "Email is required";
     if (!password) newErrors.password = "Password is required";
     if (!username) newErrors.username = "Username is required";
-    if (setErrorr) newErrors.error = "All fields are required";
+    if (!setErrors) newErrors.setErrors = "setErrors is required";
 
     if (Object.keys(newErrors).length > 0) {
-      setError(newErrors);
+      setErrors(newErrors);
       return;
     }
 
